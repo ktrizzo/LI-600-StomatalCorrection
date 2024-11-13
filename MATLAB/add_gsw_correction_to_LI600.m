53eaab00d7a3c5cc3a89c7b6a23226e785e4b2ab
ktrizzo <95317969+ktrizzo@users.noreply.github.com>
2024-11-13 15:05:10 -0800

    Changed corrections to include thermal conductance

--- MATLAB/add_gsw_correction_to_LI600.m
+++ MATLAB/add_gsw_correction_to_LI600.m
@@ -26,57 +26,64 @@ W_chambs = zeros(size(data.gsw));
 gsw_total = data.gsw.*sidedness;
 
 for i=1:length(data.gsw)
-    % --- inlet --- %
-    T_in = data.Tref(i);                                % C
-    RH_in = data.rh_r(i)/100;                           % Decimal 
-    u_in = data.flow(i)/1000;                           % mmol/s
-    P_atm = data.P_atm(i);                              % kPa
-    
-    % --- outlet --- %
-    RH_out = data.rh_s(i)/100;                          % Decimal
-    u_out = data.flow_s(i)/1000;                        % mmol/s, not used, deemed unreliable by LI-COR
+    % -- input --%
+    T_in = data.Tref(i);                        % C (inlet air temp)
+    T_leaf = data.Tleaf(i);                     % C (leaf temp)
 
-    % --- chamber --- %
-    T_leaf = data.Tleaf(i);                             % C
-    RH_chamb = RH_out;                                  % Decimal
+    RH_in = data.rh_r(i)/100;                   % Decimal (inlet RH)
+    RH_out = data.rh_s(i)/100;                  % Decimal (outlet RH)     
 
-    % -- constants -- %
-    s = 0.441786*0.01^2;                                % m^2
-    gbw = 2.921;                                        % mol/m^2/s
+    u_in = data.flow(i)/1e6;                    % mol/s (inlet air flow)
+    P_atm = data.P_atm(i);                      % kPa (air pressure)
+    s = 0.441786*0.01^2;                        % m^2 (leaf area)
+    gbw = 2.921;                                % mol/m^2/s (boundary layer conductance)
+    alpha = 0.03;                               % J/s/C (empirical thermal conductance)
 
-    syms T_out;
+    cpa = 29.14;                                % J/mol/C (air heat capacity)
+    cpw = 33.5;                                 % J/mol/C (water heat capacity)
+    lambdaw = 45502;                            % J/mol (water latent heat of vaporization)
 
-    % -- calculate inlet values -- %
-    W_in = calculateW(T_in,RH_in,P_atm);                % mol/mol
-    h_in = calculateh(T_in,W_in);                       % J/mol
-    Q_in = calculateQ(T_leaf,T_out,gbw);                % J/m^2/s
-    
-    % -- setup and solve implicit equation (4) from Bailey and Rizzo (2024) for T_out -- %
-    W_out = calculateW(T_out,RH_out,P_atm);             % mol/mol
-    h_out = calculateh(T_out,W_out);                    % J/mol 
-    equation = (s.*Q_in == u_in*1000.*(h_out - h_in));
-    T_chamb = vpasolve(equation, T_out);                % C
-    T_out_value = T_chamb;
+    a = 0.61365;                                % unitless (empirical magnitude of es vs T)
+    b = 17.502;                                 % unitless (empirical slope of es vs T)
+    c = 240.97;                                 % C (empirical offset of es vs T)
 
+    es = @(T) a*exp(b*T./(T+c));                % kPa (saturation vapor pressure vs T function)
+    W = @(T,RH) es(T).*RH./(P_atm);              % mol/mol (water vapor mole fraction)
+    Wd = @(T,RH) es(T).*RH./(P_atm-es(T).*RH);   % mol/mol (humidity ratio)
+    h = @(T,RH) cpa*T+Wd(T,RH).*(lambdaw + cpw*T); % J/mol (enthalpy) 
+
+    % -- computation -- %
+    syms Tout E gsw;
     % -- ASSUMPTION:The chamber air temperature is the average of the inlet and outlet air temperatures -- %
-    T_chamb = 0.5*(T_in+T_chamb);                       % C
-    
-    W_chamb = calculateW(T_chamb,RH_chamb,P_atm);       % mol/mol
-    W_leaf = calculateW(T_leaf,1,P_atm);                % mol/mol
-    E = (u_in.*(W_chamb-W_in))./(s.*(1-W_chamb));       % mmol/m^2/s
-    gtw = E./(W_leaf-W_chamb)/1000;                     % mol/m^2/s
-    gsw_bottom(i) = 1./(1./gtw-1./gbw);                 % mol/m^2/s
-    
-    gsw_total(i) = gsw_bottom(i).*sidedness(i);         % mol/m^2/s
-    T_chambs(i) = T_chamb;                              % C
-    W_chambs(i) = W_chamb;                              % mol/mol
-    T_outs(i) = T_out_value;                            % C
+    T_chamb = 0.5*(T_in+Tout);
+    % -- ASSUMPTION:The chamber relative humidity is the average of the inlet and outlet relative humidites -- %
+    RH_chamb = 0.5*(RH_in+RH_out);
+    W_chamb = W(T_chamb,RH_chamb);
+    W_in = W(T_in,RH_in);
+    W_out = W(Tout,RH_out);
+    W_leaf = W(T_leaf,1.0);
+    h_in = h(T_in,RH_in);
+    h_out = h(Tout,RH_out);
+    Q = alpha.*(T_in - T_chamb);
+    gtw = (gsw*gbw)./(gsw+gbw);
+    % -- solve implicit system of equations (4,7,8) from Bailey and Rizzo (2024) for T_out, E, gsw -- %
+    eq1 = ( E == gtw.*(W_leaf-W_chamb) );
+    eq2 = ( E == s.^(-1)*u_in*(W_out - W_in)*(1-W_out).^(-1) );
+    eq3 = ( E == s.^(-1).*((Q + u_in.*h_in)./(h_out)-u_in) );
+    [T_out, E_out, gsw_out]  = solve(eq1,eq2,eq3,Tout,E,gsw);
+   
+    E = double(E_out);                            % mol/m^2/s
+    gsw_bottom(i) = double(gsw_out);              % mol/m^2/s
+    gsw_total(i) = gsw_bottom(i).*sidedness(i);   % mol/m^2/s
+    T_chambs(i) = subs(T_chamb,T_out);            % C
+    T_outs(i) = T_out;                            % C
+    W_chambs(i) = double(subs(W_chamb,T_out));    % mol/mol
 end
 
 data.gsw_corrected = gsw_total;
 data.Ta_chamb_corrected = T_chambs;
-data.Wa_chamb_corrected = W_chambs;
 data.T_out_corrected = T_outs;
+data.W_chamb_corrected = W_chambs;
 data.stomatal_sidedness = sidedness;
 
 [p,f,e]=fileparts(filepath);
@@ -85,22 +92,3 @@ writetable(data,filename+"_corrected"+e);
 
 end
 
-function W = calculateW(T,RH,P)
-a = 0.61365;
-b = 17.502;
-c = 240.97;
-W = a*exp(b*T./(T+c)).*RH/P; % mol/mol
-end
-
-function h = calculateh(T,W)
-cpa = 29.14; % J/mol/C
-cpw = 33.5;  % J/mol/C
-lambdaw = 45502; % J/mol
-h = cpa*T+W.*(lambdaw + cpw*T);
-end
-
-function Q = calculateQ(T_in,T_out,gbw)
-cpa = 29.14; % J/mol/C
-heat_to_water = 1.08;
-Q = cpa.*gbw./heat_to_water.*(T_in-T_out); % J/m^2/s
-end
